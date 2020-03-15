8b71a7f4fb75c2afa089fd56a2ebecb0fb204d89
gen <gen1321@mail.ru>
2020-03-15 21:26:46 +0300

    test

--- Gemfile
+++ Gemfile
@@ -5,4 +5,6 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'pry', '~> 0.12.2'
+gem 'pry-doc'
 gem 'rubocop'
+gem 'rest-client'
--- action.rb
+++ action.rb
@@ -1,11 +1,31 @@
 # frozen_string_literal: true
 
 require 'pry'
+require 'pry-doc'
+require 'rest-client'
+
+TEMPO_WORKLOGS_ENDPOINT = 'https://api.tempo.io/core/3/worklogs'
+
 jira_issue = ENV['GITHUB_HEAD_REF']&.split(' ')&.first
 author = ENV['GITHUB_ACTOR']
 
 auth_hash =
-  ARGV[0].split(',').map { |x| x.split(":")}
-    .reduce({}) { |acc, v| acc[v[0]] = v[1]; acc }
+  ARGV[0]
+  .split(',')
+  .map { |x| x.split(':') }
+  .each_with_object({}) { |v, acc| acc[v[0]] = v[1] }
+
 p auth_hash
-author_auth_key = auth_hash[author]
+
+author_oauth_key = auth_hash[author]
+
+request_body = {
+  "issueKey": jira_issue,
+  "timeSpentSecconds": 1000
+}.to_json
+
+puts RestClient.post(
+  TEMPO_WORKLOGS_ENDPOINT,
+  request_body,
+  content_type: 'json', 'Authorization' => "Bearer #{author_oauth_key}"
+)
