11e5c8a7a9b6d8d7c5392c0b95066022ac41ba1a
gen <gen1321@mail.ru>
2020-03-15 19:13:42 +0300

    test

--- action.rb
+++ action.rb
@@ -4,6 +4,8 @@ require 'pry'
 jira_issue = ENV['GITHUB_HEAD_REF']&.split(' ')&.first
 author = ENV['GITHUB_ACTOR']
 
-auth_hash = ARGV.map { |x| x.split('=') }
+auth_hash =
+  ARGV[0].split(',').map { |x| x.split(":")}
+    .reduce({}) { |acc, v| acc[v[0]] = v[1]; acc }
 p auth_hash
 author_auth_key = auth_hash[author]
